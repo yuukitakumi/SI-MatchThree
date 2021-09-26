c1c9d91e1cc4de247d2b54f5bb5ad2c715eb9b2c
Muhammad Aulia Rahman <meehchannel7@gmail.com>
2021-09-26 20:31:15 +0700

    1.0
    Final

---
+++ Assets/Scripts/UITime.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class UITime : MonoBehaviour
+{
+    public Text time;
+    // Update is called once per frame
+    private void Update()
+    {
+        time.text = GetTimeString(TimeManager.Instance.GetRemainingTime() + 1);
+    }
+
+    public void Show()
+    {
+        gameObject.SetActive(true);
+    }
+
+    public void Hide()
+    {
+        gameObject.SetActive(false);
+    }
+
+    private string GetTimeString(float timeRemaining)
+    {
+        int minute = Mathf.FloorToInt(timeRemaining / 60);
+        int second = Mathf.FloorToInt(timeRemaining % 60);
+
+        return string.Format("{0} : {1}", minute.ToString(), second.ToString());
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+   
+}
